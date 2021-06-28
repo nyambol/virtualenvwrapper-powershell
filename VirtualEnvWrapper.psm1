f286ff715258d15cb3b64d78acce4fce0a179450
Michael Powe <michael@trollope.org>
2021-06-28 18:28:50 -0400

    updated documentation, added exports to module

--- VirtualEnvWrapper.psm1
+++ VirtualEnvWrapper.psm1
@@ -21,6 +21,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+
+#region begin setup
 $WORKON_HOME=$Env:WORKON_HOME
 $VIRTUALENWRAPPER_HOOK_DIR=''
 $Version = "0.1.4"
@@ -36,40 +38,43 @@ if ((Test-Path $WORKON_HOME) -eq $false) {
     mkdir $WORKON_HOME
 }
 
+#endregion
+
+#region begin internal functions
+
+### This function is not exported.###
 #
 # Get the absolute path for the environment
-#
 function Get-FullPyEnvPath($pypath) {
     return ("{0}\{1}" -f $WORKON_HOME, $pypath)
 }
 
+### This function is not exported.###
 #
-# Display a formated error message
-#
-function Write-FormatedError($err) {
+# Display a formatted error message
+function Write-FormattedError($err) {
     Write-Host
     Write-Host "  ERROR: $err" -ForegroundColor Red
     Write-Host
 }
 
+### This function is not exported.###
 #
-# Display a formated success messge
-#
-function Write-FormatedSuccess($err) {
+# Display a formatted success message
+function Write-FormattedSuccess($err) {
     Write-Host
     Write-Host "  SUCCESS: $err" -ForegroundColor Green
     Write-Host
 }
 
+### This function is not exported.###
 #
 # Retrieve the python version with the path the python exe regarding the version.
 # Python < 3.3 is for this function a Python 2 because the module venv comes with python 3.3
-#
 # Return the major version of python
-#
 function Get-PythonVersion($Python) {
     if (!(Test-Path $Python)) {
-        Write-FormatedError "$Python doesn't exist"
+        Write-FormattedError "$Python doesn't exist"
         return
     }
 
@@ -83,17 +88,17 @@ function Get-PythonVersion($Python) {
     $is_version_3 = $python_version -match "^Python\s3" -and !$is_version_2
 
     if (!$is_version_2 -and !$is_version_3) {
-        Write-FormatedError "Unknown Python Version expected Python 2 or Python 3 got $python_version"
+        Write-FormattedError "Unknown Python Version expected Python 2 or Python 3 got $python_version"
         return
     }
 
     return $(if ($is_version_2) {"2"} else {"3"})
 }
 
+### This function is not exported.###
 #
 # Common command to create the Python Virtual Environement.
 # $Command contains either the Py2 or Py3 command
-#
 function Invoke-CreatePyEnv($Command, $Name) {
     $NewEnv = Join-Path $WORKON_HOME $Name
     Write-Host "Creating virtual env... "
@@ -104,26 +109,26 @@ function Invoke-CreatePyEnv($Command, $Name) {
     $ActivatepPath = Join-Path $VEnvScritpsPath "activate.ps1"
     . $ActivatepPath
 
-    Write-FormatedSuccess "$Name virtual environment was created and your're in."
+    Write-FormattedSuccess "$Name virtual environment was created and your're in."
 }
 
+### This function is not exported.###
 #
 # Create Python Environment using the VirtualEnv.exe command
-#
 function New-Python2Env($Python, $Name)  {
     $Command = (Join-Path (Join-Path (Split-Path $Python -Parent) "Scripts") "virtualenv.exe")
-
+    
     if ((Test-Path $Command) -eq $false) {
-        Write-FormatedError "You must install virtualenv program to create the Python virtual environment '$Name'"
+        Write-FormattedError "You must install virtualenv program to create the Python virtual environment '$Name'"
         return
     }
 
     Invoke-CreatePyEnv $Command $Name
 }
 
+### This function is not exported.###
 #
 # Create Python Environment using the venv module
-#
 function New-Python3Env($Python, $Name) {
     if (!$Python) {
         $PythonExe = Find-Python
@@ -136,9 +141,9 @@ function New-Python3Env($Python, $Name) {
     Invoke-CreatePyEnv $Command $Name
 }
 
+### This function is not exported.###
 #
 # Find python.exe in the path. If $Python is given, try with the given path
-#
 function Find-Python ($Python) {
     # The path contains the python executable
     if ($Python.EndsWith('python.exe'))
@@ -161,7 +166,7 @@ function Find-Python ($Python) {
         return $false
     }
 
-    # The pas is a directory path not a executable path
+    # The path is a directory path not a executable path
     $PythonExe = Join-Path $Python "python.exe"
     if (!(Test-Path $PythonExe)) {
         return $false
@@ -170,9 +175,9 @@ function Find-Python ($Python) {
     return $PythonExe
 }
 
+### This function is not exported.###
 #
 # Create the Python Environment regardless the Python version
-#
 function New-PythonEnv($Python, $Name, $Packages, $Append) {
     $version = Get-PythonVersion $Python
 
@@ -186,24 +191,26 @@ function New-PythonEnv($Python, $Name, $Packages, $Append) {
     } elseif ($Version -eq "3") {
         New-Python3Env -Python $Python -Name $Name
     } else {
-        Write-FormatedError "This is the debug voice. I expected a Python version, got $Version"
+        Write-FormattedError "This is the debug voice. I expected a Python version, got $Version"
         RestorePath
 
     }
 }
 
+### This function is not exported.###
 function BackupPath {
     $Env:OLD_PYTHON_PATH = $Env:PYTHONPATH
 }
 
+### This function is not exported.###
 function RestorePath {
     $Env:PYTHONPATH = $Env:OLD_PYTHON_PATH
     $Env:Path = $Env:OLD_SYSTEM_PATH
 }
 
+### This function is not exported.###
 #
 # Test if there's currently a python virtual env
-#
 function Get-IsInPythonVenv($Name) {
     if ($Env:VIRTUAL_ENV) {
         if ($Name) {
@@ -220,20 +227,52 @@ function Get-IsInPythonVenv($Name) {
     return $false
 }
 
-# Now, work on a env
-function Workon {
+### This function is not exported.###
+#
+# Check if there is an environment named $Name
+function IsPyEnvExists($Name) {
+    $children = Get-ChildItem $WORKON_HOME
+
+    if ($children.Length -gt 0) {
+        for ($i=0; $i -lt $children.Length; $i++) {
+            if (([string]$children[$i]).CompareTo($Name) -eq 0) {
+                return $true
+            }
+        }
+    }
+
+    return $false
+}
+
+#endregion
+
+#region begin function exports
+
+### This function is exported.###
+function Invoke-WorkOn {
+    <#
+    .Synopsis
+     Act on an environment.
+    .Description
+     This command is a toggle. The named virtual environment is activated if not already so. Otherwise,the environment is deactivated.  Aliased to workon. 
+    .Parameter Name
+     The name of the environment to be acted on.
+    #>
     Param(
+	[parameter (Mandatory=$true,
+	 Position=0,
+	 HelpMessage="The name of the environment to activate is required.")]
         [string] $Name
     )
 
     if (!$Name) {
-        Write-FormatedError "No python venv to work on. Did you forget the -Name option?"
+        Write-FormattedError "No python venv to work on. Did you forget the -Name option?"
         return
     }
 
     $new_pyenv = Get-FullPyEnvPath $Name
     if ((Test-Path $new_pyenv) -eq $false) {
-        Write-FormatedError "The Python environment '$Name' don't exists. You may want to create it with 'MkVirtualEnv $Name'"
+        Write-FormattedError "The Python environment '$Name' don't exists. You may want to create it with 'MkVirtualEnv $Name'"
         return
     }
 
@@ -243,7 +282,7 @@ function Workon {
 
     $activate_path = "$new_pyenv\Scripts\Activate.ps1"
     if ((Test-path $activate_path) -eq $false) {
-        Write-FormatedError "Enable to find the activation script. You Python environment $Name seems compromized"
+        Write-FormattedError "Enable to find the activation script. You Python environment $Name seems compromized"
         return
     }
 
@@ -253,13 +292,30 @@ function Workon {
     $Env:VIRTUAL_ENV = "$new_pyenv"
 }
 
+### This function is exported.###
 #
 # Create a new virtual environment.
-#
 function New-VirtualEnv()
 {
+    <#
+    .Synopsis
+    Create a new virtual environment.
+    .Description
+    This command creates a new virtual environment using virtualenv. Aliased to mkvirtualenv.
+    .Parameter Name
+    The name of the new virtual environment to create.
+    .Parameter Requirement
+    Optional. A requirements file to use to install the packages for the new environment.
+    .Parameter Python
+    Optional. A directory holding the version of Python to be used.
+    .Parameter Packages
+    Optional. Optional package to install. May be repeated for multiple packages.
+    .Parameter Associate
+    Optional. Associate the new environment with an existing project.
+    #>
     Param(
-        [Parameter(HelpMessage="The virtual env name")]
+	# .PARAMETER Name. Mandatory. The name of the new virtual environment to create.
+        [Parameter(Mandatory=$true, HelpMessage="The name of the new virtual env.")]
         [string]$Name,
 
         [Parameter(HelpMessage="The requirements file")]
@@ -279,28 +335,28 @@ function New-VirtualEnv()
     )
 
     if ($Name.StartsWith("-")) {
-        Write-FormatedError "The virtual environment name couldn't start with - (minus)"
+        Write-FormattedError "The virtual environment name couldn't start with - (minus)"
         return
     }
 
     if ($Append -and !(Test-Path $Append)) {
-        Write-FormatedError "The path '$Append' doesn't exist"
+        Write-FormattedError "The path '$Append' doesn't exist"
         return
     }
 
     if (!$Name) {
-        Write-FormatedError "You must at least give me a PyEnv name"
+        Write-FormattedError "You must at least give me a PyEnv name"
         return
     }
 
     if ((IsPyEnvExists $Name) -eq $true) {
-        Write-FormatedError "There is an environment with the same name"
+        Write-FormattedError "There is an environment with the same name"
         return
     }
 
     $PythonRealPath = Find-Python $Python
     if (!$PythonRealPath) {
-        Write-FormatedError "The path to access to python doesn't exist. Python directory = $Python"
+        Write-FormattedError "The path to access to python doesn't exist. Python directory = $Python"
         return
     }
 
@@ -321,25 +377,19 @@ function New-VirtualEnv()
     }
 }
 
-
+### This function is exported.###
 #
-# Check if there is an environment named $Name
-#
-function IsPyEnvExists($Name) {
-    $children = Get-ChildItem $WORKON_HOME
-
-    if ($children.Length -gt 0) {
-        for ($i=0; $i -lt $children.Length; $i++) {
-            if (([string]$children[$i]).CompareTo($Name) -eq 0) {
-                return $true
-            }
-        }
-    }
-
-    return $false
-}
-
-function Get-VirtualEnvs {
+# list environments
+function Get-VirtualEnvs
+{
+    <#
+     .Synopsis
+     List existing virtual environments.
+     .Description
+     List environments created with virtualenv. The output gives the name of the environment and the version of Python associated with it. This list only shows environments created with virtualenv. Aliased to lsvirtualenv.
+     .Parameter none  
+    #>
+    Param()
     $children = Get-ChildItem $WORKON_HOME
     Write-Host
     Write-Host "`tPython Virtual Environments available"
@@ -377,46 +427,78 @@ function Get-VirtualEnvs {
     Write-Host
 }
 
+### This function is exported.###
 #
 # Remove a virtual environment.
-#
 function Remove-VirtualEnv {
+    <#
+    .Synopsis 
+    Delete the specified environment.
+    .Description 
+    Removes the specified environment completely, deleting the files and directories. Aliased to rmvirtualenv.
+    .Parameter Name
+    The name of an existing environment, as shown by Get-VirtualEnvs.
+    #>
     Param(
         [string]$Name
     )
 
     if ((Get-IsInPythonVenv $Name) -eq $true) {
-        Write-FormatedError "You want to destroy the Virtual Env you are in. Please type 'deactivate' before to dispose the environment before"
+        Write-FormattedError "You want to destroy the Virtual Env you are in. Please type 'deactivate' before to dispose the environment before"
         return
     }
 
     if (!$Name) {
-        Write-FormatedError "You must fill a environmennt name"
+        Write-FormattedError "You must fill a environmennt name"
         return
     }
 
     $full_path = Get-FullPyEnvPath $Name
     if ((Test-Path $full_path) -eq $true) {
         Remove-Item -Path $full_path -Recurse
-        Write-FormatedSuccess "$Name was deleted permanently"
+        Write-FormattedSuccess "$Name was deleted permanently"
     } else {
-        Write-FormatedError "$Name not found"
+        Write-FormattedError "$Name not found"
     }
 }
 
-<#
-.Synopsis
-    Get the current version of VirtualEnvWrapper
-#>
+#### This function is exported.###
+#
+# Display the version of this module.
 function Get-VirtualEnvVersion() {
+    <#
+    .Synopsis
+    Get the current version of VirtualEnvWrapper
+    .Description
+    Displays the current version of VirtualEnvWrapper being used.
+    .Parameter none
+    #>
+    
     Write-Host "Version $Version"
 }
 
-<#
-.Synopsis
-    Create a temporary environment. 
-#>
+### This function is exported.###
+#
+# Create a temporary virtual environment
 function New-TemporaryVirtualEnv() {
+    <#
+    .Synopsis
+    Create a temporary virtual environment.
+    .Description
+    Create a new temporary virtual environment. Aliased to mktmpenv.
+    .Parameter Cd
+    Change into the directory of the new environment.
+    .Parameter NoCd
+    Don't change directory of new environment.
+    .Parameter Requirement
+    Optional. A requirements file to use to install the packages for the new environment.
+    .Parameter Python
+    Optional. The directory of the version of Python to be used.
+    .Parameter Packages
+    Optional. A package to install when creating the environment. May be repeated for multiple packages.
+    .Parameter Associate
+    Optional. Associate an existing project directory with the environment created.
+    #>
     Param(
         [Parameter(HelpMessage="Change directory into the newly created virtual environment")]
         [alias("c")]
@@ -486,12 +568,20 @@ if ((est-Path -Path `"$dest_dir/README.tmpenv`") {
     }
 }
 
-#
-# Powershell alias for naming convention
+#endregion 
+
+#region begin export members
+## Powershell alias for naming convention
 #
 Set-Alias lsvirtualenv Get-VirtualEnvs
 Set-Alias rmvirtualenv Remove-VirtualEnv
 Set-Alias mkvirtualenv New-VirtualEnv
 Set-Alias mktmpenv New-TemporaryVirtualEnv
+Set-Alias workon Invoke-WorkOn
 
-Write-Host "Virtual Env Wrapper for Powershell activated"
+Export-ModuleMember -Function Get-VirtualEnvs, New-VirtualEnv, Remove-VirtualEnv, Invoke-WorkOn, Get-VirtualEnvVersion, New-TemporaryVirtualEnv
+Export-ModuleMember -Alias lsvirtualenv, rmvirtualenv, mkvirtualenv, mktmpenv, workon
+
+#endregion
+
+Write-Host "Activated Virtual Env Wrapper for Powershell, $Version"
